28b8423f64f2d51d9cb5ebf56f505b4389bb575f
farmazonne <farmazonne@gmail.com>
2019-10-15 10:39:29 +0300

    first commit - test.py was added

---
+++ src/test.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome("C:/Users/farmazonne/Desktop/PythonCourse/chromedriver")
+driver.get("https://www.wikipedia.org/")
+
+#controls
+search_field = driver.find_element_by_id('searchInput')
+search_button = driver.find_element_by_xpath('//button[@type = "submit"]')
+
+#actions
+"""Basic action on page"""
+search_field.send_keys('Test Automation')
+search_button.click()
+
+#check
+assert 'Test automation' in driver.title
+
+#afterAll
+driver.quit()
